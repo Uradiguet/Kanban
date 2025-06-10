dca22989e2e83171e7e9ba01199ebbf4e5caa45f
Ugo <144342579+Uradiguet@users.noreply.github.com>
2025-06-10 16:05:28 +0200

    Update UserController.kt

--- my-kanban/src/main/kotlin/fr/caensup/kanban/controllers/UserController.kt
+++ my-kanban/src/main/kotlin/fr/caensup/kanban/controllers/UserController.kt
@@ -6,49 +6,42 @@ import fr.caensup.kanban.repositories.UserRepository
 import fr.caensup.kanban.services.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
-@CrossOrigin(origins = ["http://127.0.0.1:3000"])
+@CrossOrigin(origins = ["http://localhost:3000", "http://127.0.0.1:3000"]) // CORRECTION: Ajouter port 3000
 @RequestMapping("/users")
 class UserController(
     private val userService: UserService,
     private val userRepository: UserRepository
-){
-    @GetMapping(path = ["/",""])
-    fun findAll() = userService.findAll()
+) {
+    @GetMapping
+    fun findAll() = ResponseEntity.ok(userService.findAll()) // CORRECTION: Wrapper dans ResponseEntity
 
-    @PostMapping(path = ["/",""])
-    fun save(@RequestBody user:UserDto):ResponseEntity<User>{
-        val savedUser=userService.save(user)
+    @PostMapping
+    fun save(@RequestBody user: UserDto): ResponseEntity<User> {
+        val savedUser = userService.save(user)
         return ResponseEntity(savedUser, HttpStatus.CREATED)
     }
 
     @DeleteMapping("/{id}")
-    fun delete(@PathVariable id: UUID):ResponseEntity<Void> {
-        if(userRepository.existsById(id)) {
+    fun delete(@PathVariable id: UUID): ResponseEntity<Void> {
+        return if (userRepository.existsById(id)) {
             userService.deleteById(id)
-            return ResponseEntity(HttpStatus.OK)
+            ResponseEntity.noContent().build() // CORRECTION: 204 No Content au lieu de 200
+        } else {
+            ResponseEntity.notFound().build()
         }
-        return ResponseEntity.notFound().build()
     }
 
     @PutMapping("/{id}")
-    fun update(@PathVariable id:UUID,@RequestBody user:UserDto):ResponseEntity<User>{
-      if(userRepository.existsById(id)){
-          return ResponseEntity.ok(userService.save(user))
-      }
-        return ResponseEntity.notFound().build()
-
+    fun update(@PathVariable id: UUID, @RequestBody user: UserDto): ResponseEntity<User> {
+        return if (userRepository.existsById(id)) {
+            user.id = id
+            ResponseEntity.ok(userService.save(user))
+        } else {
+            ResponseEntity.notFound().build()
+        }
     }
-
 }
