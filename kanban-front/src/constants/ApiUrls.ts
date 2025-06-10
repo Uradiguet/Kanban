444c27a17205b285e95fe33ba496d7a19d3d8d93
Ugo <144342579+Uradiguet@users.noreply.github.com>
2025-06-10 16:06:27 +0200

    Update ApiUrls.ts

--- kanban-front/src/constants/ApiUrls.ts
+++ kanban-front/src/constants/ApiUrls.ts
@@ -1,12 +1,21 @@
 const baseURL = 'http://localhost:8080';
 
 const API_URLS = {
+    // Users - OK, pas de changement
     users: `${baseURL}/users`,
+    
+    // Tasks - Endpoints complets
     tasks: `${baseURL}/tasks`,
-    boards: `${baseURL}/boards`,
-    assignTask: (taskId: string) => `${baseURL}/tasks/${taskId}/assign`,
     moveTask: (taskId: string) => `${baseURL}/tasks/${taskId}/move`,
+    assignTask: (taskId: string) => `${baseURL}/tasks/${taskId}/assign`,
+    
+    // Boards - NOUVEAUX endpoints
+    boards: `${baseURL}/boards`,
     boardTasks: (boardId: string) => `${baseURL}/boards/${boardId}/tasks`,
+    
+    // Projects - OK, existants
+    projects: `${baseURL}/projects`,
+    projectMembers: (projectId: string) => `${baseURL}/projects/${projectId}/members`,
 };
 
 export default API_URLS;
