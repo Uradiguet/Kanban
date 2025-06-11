2358f3791ff665c619c42599844b76740b6bccb2
Ugo <ugo.radiguet@sts-sio-caen.info>
2025-06-11 19:50:48 +0200

    Revert "Update ApiUrls.ts"
    This reverts commit 444c27a17205b285e95fe33ba496d7a19d3d8d93.

--- kanban-front/src/constants/ApiUrls.ts
+++ kanban-front/src/constants/ApiUrls.ts
@@ -1,21 +1,12 @@
 const baseURL = 'http://localhost:8080';
 
 const API_URLS = {
-    // Users - OK, pas de changement
     users: `${baseURL}/users`,
-    
-    // Tasks - Endpoints complets
     tasks: `${baseURL}/tasks`,
-    moveTask: (taskId: string) => `${baseURL}/tasks/${taskId}/move`,
-    assignTask: (taskId: string) => `${baseURL}/tasks/${taskId}/assign`,
-    
-    // Boards - NOUVEAUX endpoints
     boards: `${baseURL}/boards`,
+    assignTask: (taskId: string) => `${baseURL}/tasks/${taskId}/assign`,
+    moveTask: (taskId: string) => `${baseURL}/tasks/${taskId}/move`,
     boardTasks: (boardId: string) => `${baseURL}/boards/${boardId}/tasks`,
-    
-    // Projects - OK, existants
-    projects: `${baseURL}/projects`,
-    projectMembers: (projectId: string) => `${baseURL}/projects/${projectId}/members`,
 };
 
 export default API_URLS;
